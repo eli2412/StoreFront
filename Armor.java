913b786cf46ededa974da5272eeb0a4b34f23e02
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-14 14:14:17 -0700

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -3,15 +3,28 @@ package Milestone239;
 public class Armor extends SalableProduct {
     private int defense;
 
+    /**
+     * @param name
+     * @param description
+     * @param price
+     * @param quantity
+     * @param defense
+     */
     public Armor(String name, String description, double price, int quantity, int defense) {
         super(name, description, price, quantity);
         this.defense = defense;
     }
 
+    /**
+     * @return defense
+     */
     public int getDefense() {
         return defense;
     }
 
+    /**
+     * @param defense
+     */
     public void setDefense(int defense) {
         this.defense = defense;
     }
