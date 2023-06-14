b5c7438a3efd16de1f0d9db51aa0764ae010d295
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-14 14:16:31 -0700

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -3,15 +3,28 @@ package Milestone239;
 public class Weapon extends SalableProduct {
     private int damage;
 
+    /**
+     * @param name
+     * @param description
+     * @param price
+     * @param quantity
+     * @param damage
+     */
     public Weapon(String name, String description, double price, int quantity, int damage) {
         super(name, description, price, quantity);
         this.damage = damage;
     }
 
+    /**
+     * @return damage
+     */
     public int getDamage() {
         return damage;
     }
 
+    /**
+     * @param damage
+     */
     public void setDamage(int damage) {
         this.damage = damage;
     }
