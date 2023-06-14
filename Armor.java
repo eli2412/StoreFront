c789c6fb62f8841a67c53a3d12dc072e2b2d92f4
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-13 23:16:04 -0700

    Create Armor.java

---
+++ Armor.java
@@ -0,0 +1,18 @@
+package Milestone239;
+
+public class Armor extends SalableProduct {
+    private int defense;
+
+    public Armor(String name, String description, double price, int quantity, int defense) {
+        super(name, description, price, quantity);
+        this.defense = defense;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+}
