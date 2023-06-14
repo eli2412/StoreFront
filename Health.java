77f1a8f86981abe617a3a1db1c5c25210b21c9e2
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-14 14:15:45 -0700

    Update Health.java

--- Health.java
+++ Health.java
@@ -3,15 +3,28 @@ package Milestone239;
 public class Health extends SalableProduct {
     private int healingPoints;
 
+    /**
+     * @param name
+     * @param description
+     * @param price
+     * @param quantity
+     * @param healingPoints
+     */
     public Health(String name, String description, double price, int quantity, int healingPoints) {
         super(name, description, price, quantity);
         this.healingPoints = healingPoints;
     }
 
+    /**
+     * @return healingPoints
+     */
     public int getHealingPoints() {
         return healingPoints;
     }
 
+    /**
+     * @param healingPoints
+     */
     public void setHealingPoints(int healingPoints) {
         this.healingPoints = healingPoints;
     }
