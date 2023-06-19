f9d1a3ab60dfc55e9051d4710bf04cbd178d39c7
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-18 23:23:34 -0700

    Update StoreFront.java

--- StoreFront.java
+++ StoreFront.java
@@ -1,5 +1,6 @@
 package Milestone239;
 
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,8 @@ import java.util.Scanner;
  */
 public class StoreFront {
 	private static final String STORE_NAME = "PAWN SHOP";
+	private static final String LINE_SEPARATOR = "-----------------------------";
+    private static final String MENU_SEPARATOR = "=============================";
 	private InventoryManager inventoryManager;
     private ShoppingCart shoppingCart;
 
@@ -32,12 +35,14 @@ public class StoreFront {
         Armor armor1 = new Armor("Helmet", "A protective helmet", 29.99, 10, 5);
         Armor armor2 = new Armor("Chestplate", "A sturdy chestplate", 39.99, 7, 8);
         Health health1 = new Health("Potion", "A healing potion", 9.99, 20, 20);
+        Health health2 = new Health("Elixir", "Powerful healing elixir", 100, 10, 100);
 
         inventoryManager.addProduct(weapon1);
         inventoryManager.addProduct(weapon2);
         inventoryManager.addProduct(armor1);
         inventoryManager.addProduct(armor2);
         inventoryManager.addProduct(health1);
+        inventoryManager.addProduct(health2);
     }
     
     /**
@@ -45,107 +50,166 @@ public class StoreFront {
      */
     public void displayWelcomeMessage() {
         System.out.println("Welcome to " + STORE_NAME);
+        System.out.println(LINE_SEPARATOR);
     }
     
     /**
-     * Options for user
+     * Options for user display
      */
     public void displayMenu() {
         System.out.println("\nActions:");
         System.out.println("1. View Inventory");
         System.out.println("2. Add Item to Cart");
         System.out.println("3. Remove Item from Cart");
-        System.out.println("4. Purchase Items");
-        System.out.println("5. Cancel Purchase");
-        System.out.println("0. Exit");
+        System.out.println("4. View Cart");
+        System.out.println("5. Empty Cart");
+        System.out.println("6. Purchase Items");
+        System.out.println("7. Exit Store");
+        System.out.println(LINE_SEPARATOR);
+        System.out.print("Enter your choice: ");
     }
 
     /**
-     * @param choices
+     * calls from specific user input
      */
-    public void executeAction(int choice) {
-        switch (choice) {
-            case 1:
-                displayInventory();
-                break;
-            case 2:
-                addItemToCart();
-                break;
-            case 3:
-                removeItemFromCart();
-                break;
-            case 4:
-                purchaseItems();
-                break;
-            case 5:
-                cancelPurchase();
-                break;
-            case 0:
-                System.out.println("Thank you for coming to PAWN SHOP. Goodbye!");
-                System.exit(0);
-            default:
-                System.out.println("Invalid choice. Please try again.");
-        }
-    }
-    
-    /**
-     * display inventory of items
-     */
-    public void displayInventory() {
-        List<SalableProduct> inventory = inventoryManager.getInventory();
-        if (inventory.isEmpty()) {
-            System.out.println("The inventory is empty.");
-        } else {
-            System.out.println("Inventory:");
-            for (SalableProduct product : inventory) {
-                System.out.println(product.getName() + " - $" + product.getPrice() + " (Quantity: " + product.getQuantity() + ")");
-            }
-        }
-    }
-    
-    /**
-     * items selected added to cart
-     */
-    public void addItemToCart() {
+    public void start() {
         Scanner scanner = new Scanner(System.in);
-        displayInventory();
-        System.out.print("Enter the name of the product to add to the cart: ");
+        int choice;
+
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    viewInventory();
+                    break;
+                case 2:
+                    addItemToCart();
+                    break;
+                case 3:
+                    removeItemFromCart();
+                    break;
+                case 4:
+                    viewCart();
+                    break;
+                case 5:
+                    emptyCart();
+                    break;
+                case 6:
+                	purchaseItems();
+                	break;
+                case 7:
+                    System.out.println("Thank you for shopping!");
+                    break;
+                
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+
+            System.out.println(MENU_SEPARATOR);
+        } while (choice != 7);
+
+        scanner.close();
+    }
+    /**
+     * returns stock inventory
+     */
+    private void viewInventory() {
+        System.out.println("Store Inventory:");
+        System.out.println(LINE_SEPARATOR);
+
+        List<SalableProduct> inventory = inventoryManager.getInventory();
+        for (SalableProduct product : inventory) {
+            System.out.println(product.getName() + " - $" + product.getPrice() + " - " + product.getQuantity() + " in stock");
+            System.out.println("Description: " + product.getDescription());
+            System.out.println(LINE_SEPARATOR);
+        }
+    }
+
+    /**
+     * add items to cart
+     */
+    private void addItemToCart() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the product to add: ");
         String productName = scanner.nextLine();
 
-        SalableProduct product = inventoryManager.findProductByName(productName);
-        if (product == null) {
-            System.out.println("Product not found: " + productName);
+        List<SalableProduct> inventory = inventoryManager.getInventory();
+        SalableProduct product = findProductByName(productName, inventory);
+
+        if (product != null) {
+            if (product.getQuantity() > 0) {
+                shoppingCart.addItem(product);
+                product.setQuantity(product.getQuantity() - 1);
+                System.out.println("Item added to cart.");
+            } else {
+                System.out.println("Sorry, the product is out of stock.");
+            }
         } else {
-            shoppingCart.addItem(product);
-            System.out.println("Item added to cart: " + product.getName());
+            System.out.println("Product not found in the inventory.");
         }
     }
 
     /**
-     * checks for items in cart and selects item for removal
+     * removes items from cart
      */
-    public void removeItemFromCart() {
+    private void removeItemFromCart() {
         Scanner scanner = new Scanner(System.in);
-        List<SalableProduct> cartItems = shoppingCart.getCartItems();
-        if (cartItems.isEmpty()) {
-            System.out.println("The cart is empty.");
-        } else {
-            System.out.println("Cart Items:");
-            for (int i = 0; i < cartItems.size(); i++) {
-                SalableProduct product = cartItems.get(i);
-                System.out.println((i + 1) + ". " + product.getName());
-            }
-            System.out.print("Enter the number of the item to remove from the cart: ");
-            int itemNumber = scanner.nextInt();
+        System.out.print("Enter the name of the product to remove: ");
+        String productName = scanner.nextLine();
 
-            if (itemNumber < 1 || itemNumber > cartItems.size()) {
-                System.out.println("Invalid item number.");
-            } else {
-                SalableProduct product = cartItems.get(itemNumber - 1);
-                shoppingCart.removeItem(product);
-                System.out.println("Item removed from cart: " + product.getName());
+        List<SalableProduct> cartItems = shoppingCart.getCartItems();
+        SalableProduct product = findProductByName(productName, cartItems);
+
+        if (product != null) {
+            shoppingCart.removeItem(product);
+            product.setQuantity(product.getQuantity() + 1);
+            System.out.println("Item removed from cart.");
+        } else {
+            System.out.println("Product not found in the cart.");
+        }
+    }
+    
+    /**
+     * returns items if in cart
+     */
+    private void viewCart() {
+        System.out.println("Shopping Cart:");
+        System.out.println(LINE_SEPARATOR);
+
+        List<SalableProduct> cartItems = shoppingCart.getCartItems();
+        double totalPrice = 0;
+
+        for (SalableProduct product : cartItems) {
+            System.out.println(product.getName() + " - $" + product.getPrice());
+            totalPrice += product.getPrice();
+        }
+
+        System.out.println(LINE_SEPARATOR);
+        System.out.println("Total Price: $" + totalPrice);
+    }
+    /**
+     * empties cart
+     */
+    private void emptyCart() {
+        shoppingCart.emptyCart();
+        System.out.println("Cart emptied.");
+    }
+    /**
+     * @param productName
+     * @param products
+     * @return product by name
+     */
+    private SalableProduct findProductByName(String productName, List<SalableProduct> products) {
+        for (SalableProduct product : products) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                return product;
             }
         }
+        return null;
     }
 
     /**
@@ -166,42 +230,13 @@ public class StoreFront {
             System.out.println("Purchase completed successfully.");
         }
     }
-
     /**
-     * checks for items and cancels purchase in cart
+     * @param starts code
      */
-    public void cancelPurchase() {
-        List<SalableProduct> cartItems = shoppingCart.getCartItems();
-        if (cartItems.isEmpty()) {
-            System.out.println("The cart is empty. Nothing to cancel.");
-        } else {
-            System.out.println("Canceled Items:");
-            for (SalableProduct product : cartItems) {
-                System.out.println(product.getName() + " - $" + product.getPrice());
-                inventoryManager.addProduct(product);
-            }
-            cartItems.clear();
-            System.out.println("Purchase canceled successfully.");
-        }
-    }
-    /*
-     * runs programs 
-     */
-    public void start() {
-        Scanner scanner = new Scanner(System.in);
-        displayWelcomeMessage();
-        initializeStore();
-
-        while (true) {
-            displayMenu();
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
-            executeAction(choice);
-        }
-    }
-
     public static void main(String[] args) {
         StoreFront storeFront = new StoreFront();
+        storeFront.displayWelcomeMessage();
+        storeFront.initializeStore();
         storeFront.start();
     }
 }
