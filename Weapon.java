859f415fc69dfcbfe4b34ad9793fde5bb60fed9d
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-25 20:10:46 -0700

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -2,6 +2,13 @@ package Milestone239;
 
 public class Weapon extends SalableProduct implements Comparable<Weapon> {
     private int damage;
+    private String type;
+    //field input
+    public Weapon() {
+        super("", "", 0, 0, "");
+        this.damage = 0;
+    }
+    
 
     /**
      * @param name
@@ -10,9 +17,10 @@ public class Weapon extends SalableProduct implements Comparable<Weapon> {
      * @param quantity
      * @param damage
      */
-    public Weapon(String name, String description, double price, int quantity, int damage) {
-        super(name, description, price, quantity);
+    public Weapon(String name, String description, double price, int quantity, int damage, String type) {
+        super(name, description, price, quantity, type);
         this.damage = damage;
+        this.type = type;
     }
 
     /**
@@ -27,6 +35,13 @@ public class Weapon extends SalableProduct implements Comparable<Weapon> {
      */
     public void setDamage(int damage) {
         this.damage = damage;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
     /**
      *search with out case sensitive
