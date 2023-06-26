4cb1700386f9b0b46c734dae1f9f142d7b2bb336
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-25 20:09:45 -0700

    Update Health.java

--- Health.java
+++ Health.java
@@ -2,6 +2,15 @@ package Milestone239;
 
 public class Health extends SalableProduct {
     private int healingPoints;
+    private String type;
+    
+    /**
+     * field inputs
+     */
+    public Health() {
+        super("", "", 0, 0, "");
+        this.healingPoints = 0;
+    }
 
     /**
      * @param name
@@ -10,9 +19,10 @@ public class Health extends SalableProduct {
      * @param quantity
      * @param healingPoints
      */
-    public Health(String name, String description, double price, int quantity, int healingPoints) {
-        super(name, description, price, quantity);
+    public Health(String name, String description, double price, int quantity, int healingPoints, String type) {
+        super(name, description, price, quantity, type);
         this.healingPoints = healingPoints;
+        this.type = type;
     }
 
     /**
@@ -27,5 +37,13 @@ public class Health extends SalableProduct {
      */
     public void setHealingPoints(int healingPoints) {
         this.healingPoints = healingPoints;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
