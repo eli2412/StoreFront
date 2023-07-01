899f2f3b4828489921e5d487add0d0981b01a3e5
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-30 21:18:32 -0700

    Update InventoryManager.java

--- InventoryManager.java
+++ InventoryManager.java
@@ -7,33 +7,34 @@ import java.util.List;
  * @author eliascruz
  *
  */
-public class InventoryManager {
-	private List<SalableProduct> inventory;
+public class InventoryManager<T extends SalableProduct> {
+	private List<T> inventory;
+
 
     /**
      * list
      */
     public InventoryManager() {
-        this.inventory = new ArrayList<>();
+    	this.inventory = new ArrayList<>();
     }
     /**
      * @param product add to inventory
      */
-    public void addProduct(SalableProduct product) {
+    public void addProduct(T product) {
         inventory.add(product);
     }
 
     /**
      * @param product removed
      */
-    public void removeProduct(SalableProduct product) {
+    public void removeProduct(T product) {
         inventory.remove(product);
     }
 
     /**
      * @return list
      */
-    public List<SalableProduct> getInventory() {
+    public List<T> getInventory() {
         return inventory;
     }
 }
