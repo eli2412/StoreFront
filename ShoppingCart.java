26829bd2be187d77bd8c580308714bd18d77710e
Elias <99225154+eli2412@users.noreply.github.com>
2023-06-30 21:22:05 -0700

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -7,34 +7,33 @@ import java.util.List;
  * @author eliascruz
  *
  */
-public class ShoppingCart {
-	private List<SalableProduct> cartItems;
-
+public class ShoppingCart<T extends SalableProduct> {
+    private List<T> cartItems;
     /**
      * cart list
      */
     public ShoppingCart() {
-        this.cartItems = new ArrayList<>();
+    	this.cartItems = new ArrayList<>();
     }
 
     /**
      * @param product add to cart
      */
-    public void addItem(SalableProduct product) {
+    public void addItem(T product) {
         cartItems.add(product);
     }
 
     /**
      * @param product remove from cart
      */
-    public void removeItem(SalableProduct product) {
+    public void removeItem(T product) {
         cartItems.remove(product);
     }
 
     /**
      * @return list
      */
-    public List<SalableProduct> getCartItems() {
+    public List<T> getCartItems() {
         return cartItems;
     }
     /**
