2484a9032e1963e4739c5564c3bb13e227d7da84
Elias <99225154+eli2412@users.noreply.github.com>
2023-07-10 15:18:35 -0700

    Update SalableProductDeserializer.java

--- SalableProductDeserializer.java
+++ SalableProductDeserializer.java
@@ -1,42 +1,42 @@
 package Milestone239;
 
-	import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 
-	import java.io.IOException;
+import java.io.IOException;
+import java.math.BigDecimal;
 
-	/**
-	 * @author eliascruz
-	 *
-	 */
-	public class SalableProductDeserializer extends StdDeserializer<SalableProduct> {
-	    public SalableProductDeserializer() {
-	        this(null);
-	    }
+public class SalableProductDeserializer extends JsonDeserializer<SalableProduct> {
+    @Override
+    public SalableProduct deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        JsonNode node = jsonParser.readValueAsTree();
+        String name = node.get("name").asText();
+        String description = node.get("description").asText();
+        double price = node.get("price").doubleValue();
+        int quantity = node.get("quantity").asInt();
+        String type = node.get("type").asText();
 
-	    public SalableProductDeserializer(Class<?> vc) {
-	        super(vc);
-	    }
+        SalableProduct product;
 
-	    @Override
-	    public SalableProduct deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-	        ObjectCodec codec = parser.getCodec();
-	        JsonNode node = codec.readTree(parser);
+        // Instantiate the appropriate subclass based on the type field
+        if ("weapon".equals(type)) {
+            int damage = node.get("damage").asInt();
+            product = new Weapon(name, description, price, quantity, damage, type);
+        } else if ("armor".equals(type)) {
+            int defense = node.get("defense").asInt();
+            product = new Armor(name, description, price, quantity, defense, type);
+        } else if ("health".equals(type)) {
+            int healingPoints = node.get("healingPoints").asInt();
+            product = new Health(name, description, price, quantity, healingPoints, type);
+        } else {
+            // Handle unknown or unsupported types
+            throw new IllegalArgumentException("Unsupported product type: " + type);
+        }
 
-	        String type = node.get("type").asText();
-	        switch (type) {
-	            case "armor":
-	                return codec.treeToValue(node, Armor.class);
-	            case "health":
-	                return codec.treeToValue(node, Health.class);
-	            case "weapon":
-	                return codec.treeToValue(node, Weapon.class);
-	            default:
-	                throw new IllegalArgumentException("Unknown product type: " + type);
-	        }
-	    }
-	}
+        return product;
+    }
+}
 
