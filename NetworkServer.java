cf66218cdd198770763dc38877a4362f6f3087a2
Elias <99225154+eli2412@users.noreply.github.com>
2023-07-10 19:59:10 -0700

    Update NetworkServer.java

--- NetworkServer.java
+++ NetworkServer.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+// listens for incoming packets on a specific port.
+// processes the command using the AdministrationService, sends a response back to the client, and retrieves the inventory JSON.
 
 public class NetworkServer implements Runnable {
-    private static final int BUFFER_SIZE = 1024;
+    private static final int BUFFER_SIZE = 1024; // size of the buffer used to receive data
     private static final int TIMEOUT = 50000; // Timeout in milliseconds
     static final int PORT = 13348;
 
@@ -18,16 +20,28 @@ public class NetworkServer implements Runnable {
         this.administrationService = administrationService;
     }
 
+    /**
+     * @return port num
+     */
     public int getPort() {
         return PORT;
     }
     
+    /**
+     *  strats server
+     */
     public void startServer() {
         Thread serverThread = new Thread(this);
         serverThread.start();
     }
     
 
+    /**
+     *Creates a new DatagramSocket and sets the timeout value using setSoTimeout().
+	 *Enters a while loop to continuously listen for incoming packets.
+	 *Receives the incoming packet and extracts the received data.
+	 *Calls the processReceivedData() method to handle the received data.
+     */
     @Override
     public void run() {
         try {
@@ -52,12 +66,20 @@ public class NetworkServer implements Runnable {
         }
     }
 
+    /**
+     * Calls the processCommand() method of the AdministrationService to handle the received command
+     * Retrieves the inventory JSON using the getInventoryJson() method of the AdministrationService
+     * Prints the response payload, which is the inventory JSON
+     * @param receivedData
+     * @param clientAddress
+     * @param clientPort
+     */
     private void processReceivedData(String receivedData, InetAddress clientAddress, int clientPort) {
         Thread processThread = new Thread(() -> {
             String[] parts = receivedData.split("\n", 2);
             String command = parts[0];
-            String payload = parts.length > 1 ? parts[1] : null;
-            System.out.println("Received payload: " + payload);
+            String payload = parts.length > 1 ? parts[1] : "";
+            System.out.println("Received payload"); //payload
             administrationService.processCommand(command);
 
             // Send the response back to the client
